79629f75fed656b86a7044b5c29785b023ffd4ac
renanluizmacedo <renanluizmacedo@gmail.com>
2021-07-18 16:28:08 -0300

    verificacao login

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
+use function Ramsey\Uuid\v1;
 
 class AdminLoginController extends Controller
 {
@@ -16,6 +19,24 @@ class AdminLoginController extends Controller
     }
 
     public function login(Request $request){
-        return "OK";
+
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ]; 
+
+        $authOK = Auth::guard('admin')->attempt($credentials, $request->remember);
+
+
+        if($authOK){
+            return redirect()->intended(route('admin.dashboard'));
+        }
+
+        return redirect()->back()->withInputs($request->only('email','remember'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/admin',[App\Http\Controllers\AdminController::class, 'index'])->name('admin.dashboard');
 
-Route::get('/admin/login',[App\Http\Controllers\Auth\AdminLoginController::class, 'index'])->name('admin.login');
-Route::post('/admin/login',[App\Http\Controllers\Auth\AdminLoginController::class, 'login'])->name('admin.login.submit');
+Route::get('/admin/login',[App\Http\Controllers\Auth\AdminLoginController::class, 'index'])
+->name('admin.login');
+
+Route::post('/admin/login',[App\Http\Controllers\Auth\AdminLoginController::class, 'login'])
+->name('admin.login.submit');
